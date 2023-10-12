2fbe49ff43b06677b0fdd3b4535b4df9db63d6f1
Léo Dana <cargodana8@gmail.com>
2023-10-12 15:03:34 +0200

    change of doc

--- hyperplane_computation/utils.py
+++ hyperplane_computation/utils.py
@@ -1,11 +1,12 @@
 #code containing all the useful functions for the notebook to run
 
 import torch
-device = "cuda"
+from concept_erasure.leace import LeaceEraser
 cosim = torch.nn.CosineSimilarity(-1)
 
 Gender = 1 | -1 #1 for male, -1 for female
 Label = str #'noun' | 'pronoun' |'name' | 'anatomy'
+Data = str
 
 
 def process_labels(gender : Gender, label : Label):
@@ -22,7 +23,7 @@ def process_labels(gender : Gender, label : Label):
     return [0, 0, gender, 0]
 
 
-def initiate_activations(dataset, with_label = True, **dict):
+def initiate_activations(dataset : list[list[Data, list[int]]], with_label = True, **dict):
   device = dict['device']
   model = dict['model']
   tokenizer = dict['tokenizer']
@@ -32,15 +33,15 @@ def initiate_activations(dataset, with_label = True, **dict):
   labels = []
 
   for batch in dataset:
-    #We keep track the length of each prompt to find the right indices on which to measure the probability.
+    #We keep track the length of each prompt to find the indices on which to measure the probability.
     indices.append(torch.Tensor([len(tokenizer(data[0])["input_ids"])-1 for data in batch]).to(int).to(device))
 
-    #initiate activations
+    #We initiate the activations manually
     tokenized_batch = tokenizer([data[0] for data in batch], padding = True, return_tensors = 'pt')["input_ids"].to(device)
-    positions = torch.Tensor([i for i in range(tokenized_batch.shape[1])]).to(int).to(device)
+    positions = torch.arrange(tokenized_batch.shape[1]).to(int).to(device).to(int).to(device) #([i for i in range(tokenized_batch.shape[1])]).to(int).to(device)
     activations.append(model.transformer.wte(tokenized_batch) + model.transformer.wpe(positions))
     if with_label:
-      labels.append(torch.Tensor([process_labels(data[1], data[2]) for data in batch]).unsqueeze(-1))
+      labels.append(torch.Tensor([data[1] for data in batch]).unsqueeze(-1))
   
   #Deletion of all the useless tensor to avoid RAM overload.
   del positions
@@ -49,7 +50,9 @@ def initiate_activations(dataset, with_label = True, **dict):
   return indices, activations, labels
 
 
-def gather_update_acts(activations, layer, post_layer_norm, indices, N_batch, **dict):
+def gather_update_acts(activations : list[torch.Tensor], layer : int, 
+                       post_layer_norm : bool, indices : list[torch.Tensor], 
+                       N_batch : int, **dict):
   model = dict['model']
   device = dict['device']
 
@@ -72,52 +75,50 @@ def gather_update_acts(activations, layer, post_layer_norm, indices, N_batch, **
   return activations, target_activations
 
 #ToDo: Implement a better version, using means of median
-def get_quantile(leace_eraser, target_activations, **dict):
+def get_quantile(leace_eraser : LeaceEraser, all_acts : torch.Tensor, **dict):
   device = dict['device']
   #We compute the parameters of the hyperplane to project against.
   hyperplane = (leace_eraser.proj_right[0]/torch.norm(leace_eraser.proj_right[0], dim = -1).unsqueeze(-1)).squeeze().to(device)
 
   #We sort the projection.
-  sorted_tensor, indices = torch.sort(torch.einsum('nd, ...d -> n...', target_activations, hyperplane), dim = -1)
+  sorted_tensor, indices = torch.sort(torch.einsum('nd, ...d -> n...', all_acts, hyperplane), dim = -1)
   del indices
 
-  Nb_ex = len(target_activations)
-  #See above for the quantile formula.
+  Nb_ex = len(all_acts)
   quantile = (sorted_tensor[Nb_ex//2 + 1] + sorted_tensor[(Nb_ex-1)//2+1])*(hyperplane)/2
 
   del sorted_tensor
-  del hyperplane
 
   return quantile
 
 
 
-def probe_eval(eraser_list):
+def probe_eval(eraser_list : list[LeaceEraser], **dict):
+  device = dict['device']
+
   def metric(target_activations, layer : int, true_label):
     dir = eraser_list[layer].proj_right[0].to(device)
     bias = eraser_list[layer].bias.to(device)
     Nb_ex = len(target_activations)
 
-    #we compute the accuracy
     acc = torch.sum(cosim(dir, target_activations - bias)*true_label > 0).item()/Nb_ex
-
-    del dir
-    del bias
-
     return acc
+  
   return metric
 
 
 
-def token_augmentation(list, text_list, tokenizer):
+def token_augmentation(list : list, text_list : list[str], **dict):
+  tokenizer = dict['tokenizer']
   for text in text_list:
     for i in range(1, len(text)):
       if (text[:i] != " ") and (text[i:] != " "):
         list.append(tokenizer(text[:i])["input_ids"] + tokenizer(text[i:])["input_ids"])
 
 
-def show_proba(proba, tokenizer, level = 0.01, nb_tokens = 10, decode = False):
-  #From a probability, get the top-{nb_tokens} tokens that have more than {level} probability.
+def show_proba(proba, level = 0.01, nb_tokens = 10, decode = False, **dict):
+  '''From a probability, get the top-{nb_tokens} tokens that have more than {level} probability.'''
+  tokenizer = dict['tokenizer']
 
   if decode:
     #to print the words
@@ -131,29 +132,30 @@ def show_proba(proba, tokenizer, level = 0.01, nb_tokens = 10, decode = False):
 
 
 
-#Finds the occurences of the target inside the examples, but only the last one for each target.
-def finds_indices(example_tokens, target_tokens):
+def finds_indices(example_tokens : list[list[int]], target_tokens : list[list[int], list[int]]):
+  '''
+  Finds the occurences of the target inside the examples, but only the last one for each target.
+  stream_indices : list[int], list of all streams where the target was detected.
+  example_indices : list[int], list of all example where the target was detected.
+  stream_example_indices : list[list[int], list[int]], list that give for each target
+  the join example and stream where it was detected.
+  '''
 
-  #stream_indice is the list of all streams where the target was detected
   stream_indices = []
-  #example_indice is the list of all example where the target was detected
   example_indices = []
-  #stream_example_indices is the list that gives the last stream of each token targeted
-  #as well as the example it was seen in
   stream_example_indices = [[],[]]
-
 
   for i, (example, target_token) in enumerate(zip(example_tokens, target_tokens)):
     len_target = len(target_token)
 
-    #If there is no target, we hook the last stream.
+    #If there is no target, we take the last stream.
     if len(target_token) == 0:
       s_indice = torch.Tensor([len(example)])
       e_indice = torch.Tensor([i])
       stream_example_indices[0].append(len(example))
       stream_example_indices[1].append(i)
 
-    #Otherwise, we hook the targeted streams.
+    #Otherwise, we take the targeted streams.
     else:
       position = torch.where(torch.Tensor(example) == target_token[0])[0][-1].item() #[-1] means that we take only the last occurence
       if [example[i] for i in range(position, position + len_target)] == target_token:
