e0353658963f285ed9558a9382fd81a390f054cf
Léo Dana <cargodana8@gmail.com>
2023-10-27 18:27:38 +0100

    new test and data functions

--- init_dataset.py
+++ init_dataset.py
@@ -1,5 +1,8 @@
 '''
 Run this file to create the Trin and Test datasets.
 '''
-import Data.Train_Data
-import Data.Test_Data
+from Data.Train_Data import generate_trainset
+from Data.Test_Data import generate_testset
+
+generate_testset()
+generate_trainset()
